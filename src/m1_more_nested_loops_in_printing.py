603bde4a2296edceb64155c67c82b2d7dba02b42
dohertre <dohertre@rose-hulman.edu>
2019-07-23 15:51:40 -0400

    rand

--- src/m1_more_nested_loops_in_printing.py
+++ src/m1_more_nested_loops_in_printing.py
@@ -55,7 +55,7 @@ def triangle_right_justified(r):
     For purposes of "lining up", assume r is a single digit.
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # HINT: Do the following problem FIRST, then convert x's to spaces:
@@ -75,10 +75,11 @@ def triangle_right_justified(r):
     # ------------------------------------------------------------------
     for k in range(r):
         for j in range(r - k - 1):
-            print(" ", end='')
+            print(' ', end='')
         for i in range(k + 1):
             print(i + 1, end='')
         print()
+
 
 def run_test_triangle_upside_down():
     """ Tests the    triangle_upside_down    function. """
@@ -114,7 +115,7 @@ def triangle_upside_down(r):
     For purposes of "lining up", assume r is a single digit.
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -122,7 +123,12 @@ def triangle_upside_down(r):
     #   in this or the other problems in this module, as doing so would
     #   defeat the goal of providing practice at loops within loops.
     # ------------------------------------------------------------------
-
+    for k in range(r):
+        for j in range(k):
+            print(' ', end='')
+        for i in range(r - k):
+            print(i + 1, end='')
+        print()
 
 def run_test_vee():
     """ Tests the    vee    function. """
