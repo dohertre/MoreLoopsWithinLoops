373acd3d2464d3a7e5d14d04f6aa7c5ed3bf4ce6
dohertre <dohertre@rose-hulman.edu>
2019-07-23 16:03:15 -0400

    rand

--- src/m1_more_nested_loops_in_printing.py
+++ src/m1_more_nested_loops_in_printing.py
@@ -305,6 +305,7 @@ def numbers_constant_backwards(r, maxnum, n):
             print(' ', end='')
         print()
 
+
 def run_test_numbers_increasing_forward():
     """ Tests the    numbers_increasing_forward    function. """
     print()
@@ -355,7 +356,12 @@ def numbers_increasing_forward(r, maxnum):
     #   in this or the other problems in this module, as doing so would
     #   defeat the goal of providing practice at loops within loops.
     # ------------------------------------------------------------------
-
+    for k in range(r):
+        for j in range(maxnum):
+            for i in range(j):
+                print(j + 1, end='')
+            print(' ', end='')
+        print()
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
